c83e23a6abb9a37504da00b1bf8de39dfa5e70b2
YashGupta404 <yashgupta.work.2005@gmail.com>
2025-06-30 00:09:08 +0530

    Model Trainer done

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 import certifi
 
-uri = "mongodb+srv://yashguptawork2005:6fO8lQBuYLQs0lXB@ac-1qzyqeu-shard-00-00.tv4zylh.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority"
 
 # Replace <username> and <password> with your real MongoDB Atlas credentials
 client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
